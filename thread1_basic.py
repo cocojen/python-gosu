6759eed3469ac6d9814ce4e7a24fab576cc0a84a
hyeonji cho <huji3156@gmail.com>
2022-02-08 22:52:56 +0900

    Add Daemon Thread

--- thread1_basic.py
+++ thread1_basic.py
@@ -3,9 +3,11 @@ import threading
 import time
 
 # 스레드 실행 함수
-def thread_func(name):
+def thread_func(name, sec):
     logging.info("Sub-Thread %s: starting", name)
-    time.sleep(3)
+    
+    time.sleep(sec)
+    
     logging.info("sub-Thread %s: finishing", name)
     
 
@@ -17,12 +19,18 @@ if __name__ == "__main__":
     logging.info("Main-Thread: before creating thread")    
 
     # 함수 인자 확인
-    x = threading.Thread(target=thread_func, args=('First',))
+    # Daemon 은 Default 값이 False 임.
+    x = threading.Thread(target=thread_func, args=('First', 3), daemon=True)
+    y = threading.Thread(target=thread_func, args=('Second', 2))
+    
+    # 데몬 스레드 확인
+    print(x.isDaemon())
     
     logging.info("Main-Thread: before running thread")
     
     # 서브 스레드 시작
     x.start()
+    y.start()
     # thread_func('first')
     
     logging.info("Main-Thread: wait for the thread to finish")
